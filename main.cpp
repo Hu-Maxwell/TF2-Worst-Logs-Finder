8a58e788d5e60a8329c819e7ce6d6a686ab34f0a
Maxwell Hu <dewebiag@gmail.com>
2024-10-11 13:57:40 -0700

    Added API that fetches DPM.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,29 @@
-#include "crow.h"
+#include <iostream>
 #include <fstream>
 #include <sstream>
-#include <iostream>
+#include <string>
 
-// Helper function to read static files
+#include "crow.h"
+#include <curl/curl.h>
+#include <nlohmann/json.hpp>
+
 std::string read_file(const std::string& filepath) {
-    std::ifstream file("static/" + filepath);  // Static directory added
+    std::ifstream file("static/" + filepath);
     if (file.is_open()) {
         std::stringstream buffer;
         buffer << file.rdbuf();
         return buffer.str();
     }
-    std::cerr << "File not found: static/" << filepath << std::endl;  // Debug output
+    std::cerr << "File not found: static/" << filepath << std::endl;
     return "File not found.";
 }
+
+static size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* buffer) {
+    size_t newLength = size * nmemb;
+    buffer->append((char*)contents, newLength);
+    return newLength;
+}
+
 
 int main() {
     crow::SimpleApp app;
@@ -46,9 +56,47 @@ int main() {
         return res;
     });
 
+    std::string logID = "3732357";
+    std::string playerUID = "[U:1:431667580]"; 
+ 
+    // http://logs.tf/api/v1/log?title=X&uploader=Y&player=Z&limit=N&offset=N 
+    std::string apiUrl = "https://logs.tf/api/v1/log/" + logID;
 
-    CROW_ROUTE(app, "/api/helloworld")([](){
-        return crow::response("Hello, world!");
+    // dewe's UID
+    // [U:1:431667580]
+
+ 
+    CURL* curl;
+    CURLcode res;
+    std::string responseString;
+ 
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+
+    std::string DPMString;
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, apiUrl.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseString);
+ 
+        res = curl_easy_perform(curl);
+ 
+        if(res != CURLE_OK) {
+            std::cerr << "Failed." << curl_easy_strerror(res) << std::endl;
+        } else {
+            auto jsonResponse = nlohmann::json::parse(responseString);
+ 
+            float DPM = jsonResponse["players"][playerUID]["dapm"];
+            DPMString = "The DPM you hit in this log was " + std::to_string(DPM) + ".";
+         }
+ 
+        curl_easy_cleanup(curl);
+    }
+ 
+    curl_global_cleanup();
+
+    CROW_ROUTE(app, "/api/helloworld")([&DPMString](){
+        return crow::response(DPMString.c_str());
     });
 
     // Start the Crow server on port 8080
